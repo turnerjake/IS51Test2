6369a0ec5ee913559117b43538250fa7b6e3a4cb
turnerjake <jaketurner@mail.fresnostate.edu>
2020-12-03 16:25:04 -0800

    Structured english

---
+++ test2.py
@@ -0,0 +1,17 @@
+"""
+STRUCTURED ENGLISH
+
+Open file 'final.txt'
+Read through each line of the file
+Create a list with each grade found on every line
+Get the length of the list and print it to the user
+
+Find sum of list
+Divide sum by length and print result (the average) to user
+
+Iterate through list and find # of items above our found average
+Divide this count by the total length of list and print result to user.
+This is the % of scores above the average
+
+END STRUCTURED ENGLISH
+"""
